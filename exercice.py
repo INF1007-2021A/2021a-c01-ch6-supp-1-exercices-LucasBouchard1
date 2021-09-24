8da32203f7404d76d2b5dd286c945b458bee894e
LucasBouchard1 <lucas-2.bouchard@polymtl.ca>
2021-09-24 11:18:45 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -7,17 +7,18 @@ import copy
 import itertools
 
 
-def get_maximums(numbers):
-	return [0]
+get_maximums = lambda numbers : [max(x) for x in numbers]
 
-def join_integers(numbers):
-	return 0
+join_integers = lambda numbers : int("".join([str(x) for x in numbers]))
 
 def generate_prime_numbers(limit):
-	return [0]
+	nombres, premiers = [x for x in range(2,limit+1)], []
+	while nombres:
+		premiers.append(nombres.pop(0))
+		nombres = [x for x in nombres if x%premiers[-1]]
+	return premiers
 
-def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+combine_strings_and_numbers = lambda strings, num_combinations, excluded_multiples=1 : [x+str(y) for y in range(1, num_combinations+1, excluded_multiples) for x in strings ]
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
@@ -27,5 +28,5 @@ if __name__ == "__main__":
 	print("")
 	print(generate_prime_numbers(17))
 	print("")
-	print(combine_strings_and_numbers(["A", "B"], 2, None))
+	print(combine_strings_and_numbers(["A", "B"], 2))
 	print(combine_strings_and_numbers(["A", "B"], 5, 2))
